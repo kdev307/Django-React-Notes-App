aa7af9c309fc7fca88a0b54ee1d97f170b290a45
kdev307 <127479436+kdev307@users.noreply.github.com>
2024-09-07 16:59:48 +0530

    Updations made

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -98,14 +98,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE' : 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER' : os.getenv("DB_USER"),
-        'PASSWORD' : os.getenv("DB_PWD"),
-        'HOST' : os.getenv("DB_HOST"),
-        'PORT' : os.getenv("DB_PORT")
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # Below are used for deploying the project using choreo (I'm skipping it)
+        # 'ENGINE' : 'django.db.backends.postgresql',
+        # 'NAME': os.getenv("DB_NAME"),
+        # 'USER' : os.getenv("DB_USER"),
+        # 'PASSWORD' : os.getenv("DB_PWD"),
+        # 'HOST' : os.getenv("DB_HOST"),
+        # 'PORT' : os.getenv("DB_PORT")
     }
 }
 

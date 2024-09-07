8654f410e060c647aa49445d8a18ca89e239c654
kdev307 <127479436+kdev307@users.noreply.github.com>
2024-09-07 16:48:06 +0530

    first commit

---
+++ backend/api/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    # auto_now_add helps in automatically populating a particular data rather than filling it
+    created_at = models.DateTimeField(auto_now_add=True)
+    # models.CASCADE says that we should also delete all of the notes this user when we are deleting the data source (User in this case) using on_delete
+    # related_name tells the name which will be the references to the notes (helps in accessing as .notes)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="notes")
+
+    def __str__(self):
+        return self.title
---
+++ backend/api/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer, NotesSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Note
+
+# Create your views here.
+
+# ListCreateAPIView will either list all the notes of the user or create a new note
+class NoteListCreate(generics.ListCreateAPIView):
+    serializer_class = NotesSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
+    
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+        else:
+            print(serializer.errors)
+
+class NoteDelete(generics.DestroyAPIView):
+    serializer_class = NotesSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
+
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]

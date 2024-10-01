4ea5f421d24aa1a20b8f1a8ce098ee6ed065e294
kdev307 <127479436+kdev307@users.noreply.github.com>
2024-10-01 20:38:59 +0530

    Customized user registration

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
 import Form from "../components/Form";
 
 function Login() {
-    return <Form route="/api/token/" method="login" />;
+    return (
+        <>
+            <Form route="/api/token/" method="login" />
+            <p className="para-info">
+                {`Don't have account ?`} <Link to="/register">{`Register Here`}</Link>
+            </p>
+        </>
+    );
 }
 
 export default Login;
